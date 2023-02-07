71606f87bd5940899379e3ae410b37a2665411f9
virgiawankusuma <virgiawan150@gmail.com>
2023-02-07 11:17:45 +0700

    Build

---
+++ .next/server/pages/api/data/en/PhotographedData.js
@@ -0,0 +1,97 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 140;
+exports.ids = [140];
+exports.modules = {
+
+/***/ 7453:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "benefitsDataEn": () => (/* binding */ benefitsDataEn),
+/* harmony export */   "ctaDataEn": () => (/* binding */ ctaDataEn),
+/* harmony export */   "featuredDataEn": () => (/* binding */ featuredDataEn),
+/* harmony export */   "reinforcementQuoteEn": () => (/* binding */ reinforcementQuoteEn)
+/* harmony export */ });
+const reinforcementQuoteEn = ()=>[
+        {
+            id: 0,
+            heading: "Research from Market Research Future (MRF)",
+            text: "reveals that by 2025, the virtual reality industry will increase to 73.7% of CAGR."
+        },
+        {
+            id: 1,
+            heading: "Virtual Reality helps increase Sales Potential",
+            text: "With a virtual reality tour, you can implement a “try before you buy” marketing strategy. Take your potential customers around virtually. Show the best spot you have. An engaging sensory experience triggers increased sales potential."
+        }, 
+    ];
+const benefitsDataEn = ()=>[
+        {
+            id: 0,
+            content: "Make your business look more real in a virtual tour."
+        },
+        {
+            id: 1,
+            content: "Tell your business services and facilities more informatively."
+        },
+        {
+            id: 2,
+            content: "24-hour access makes it easy for your visitors to find the best place to vacation with just a 360 visual."
+        },
+        {
+            id: 3,
+            content: "Show the best spots of your business unlimited in virtual environment from live image capture."
+        }, 
+    ];
+const featuredDataEn = ()=>[
+        {
+            id: 0,
+            image: "/img/virtual-reality/photographed/hotel.webp",
+            title: "Hotel/Villa",
+            content: "Show a comfortable stay in the best rooms. Virtual Tour 360 shows the best details and spots that normal photos cannot. Describe your best facilities and services through an environment created from live image capture."
+        },
+        {
+            id: 1,
+            image: "/img/virtual-reality/photographed/resto.webp",
+            title: "Restaurant/Cafe",
+            content: "Give an extraordinary experience to your visitors not only for delicious food but a comfortable and instagramable place through a virtual tour. Tell the best spots of your restaurant/cafe in a realistic virtual environment."
+        },
+        {
+            id: 2,
+            image: "/img/virtual-reality/photographed/toko.webp",
+            title: "Retail Store",
+            content: "Give the virtual shopping experience through 360 photographs of your retail store with real experience. Show the completeness of your product collection anytime and anywhere. Make your retail store promotion more modern than others."
+        },
+        {
+            id: 3,
+            image: "/img/virtual-reality/photographed/wisata.webp",
+            title: "Tourist Attraction",
+            content: "Visualize more real the beauty of the tourist attractions that you have. Attractive promotions will bring in many visitors from anywhere. Help your visitors find their favorite photo spots through a virtual tour."
+        }, 
+    ];
+const ctaDataEn = ()=>[
+        {
+            id: 0,
+            title: "Ready to transform your business with us?",
+            text: "",
+            button: "Get it Now"
+        }, 
+    ];
+
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(7453));
+module.exports = __webpack_exports__;
+
+})();
---
+++ .next/server/pages/api/data/id/HomeData.js
@@ -0,0 +1,236 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 282;
+exports.ids = [282];
+exports.modules = {
+
+/***/ 6290:
+/***/ ((module) => {
+
+module.exports = require("react-icons/fa");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 171:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "benefitButtonsData": () => (/* binding */ benefitButtonsData),
+/* harmony export */   "benefitsData": () => (/* binding */ benefitsData),
+/* harmony export */   "clientsData": () => (/* binding */ clientsData),
+/* harmony export */   "ctaData": () => (/* binding */ ctaData),
+/* harmony export */   "featuresData": () => (/* binding */ featuresData),
+/* harmony export */   "portfolioData": () => (/* binding */ portfolioData),
+/* harmony export */   "virtualRealityData": () => (/* binding */ virtualRealityData)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_icons_fa__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6290);
+/* harmony import */ var react_icons_fa__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_icons_fa__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const clientsData = ()=>[
+        {
+            id: 0,
+            name: "Decorunic",
+            logo: "decorunic.webp"
+        },
+        {
+            id: 1,
+            name: "Decorunic Project",
+            logo: "decorunic-project.webp"
+        },
+        {
+            id: 2,
+            name: "Woodenic",
+            logo: "woodenic.webp"
+        },
+        {
+            id: 3,
+            name: "CV. Ruang Inspirasi Indonesia",
+            logo: "cvruang-inspirasi.webp"
+        },
+        {
+            id: 4,
+            name: "Firda Dental Care",
+            logo: "firda.webp"
+        },
+        {
+            id: 5,
+            name: "Sekuro Village Beach Resort",
+            logo: "sekuro.webp"
+        },
+        {
+            id: 6,
+            name: "Seaside Villa & Muse Beach Resto",
+            logo: "seaside.webp"
+        }, 
+    ];
+const portfolioData = ()=>[
+        {
+            id: 0,
+            title: "Decorunic Virtual Showroom",
+            url: "https://vrtour.decorunic.id"
+        },
+        {
+            id: 1,
+            title: "Sekuro Village Beach Resort Virtual Reality",
+            url: "https://sekurovillage.digidaxa.com"
+        },
+        {
+            id: 2,
+            title: "Firda Dental Care Virtual Reality",
+            url: "https://firdadentalcare.digidaxa.com"
+        },
+        {
+            id: 3,
+            title: "Seaside Villa & Muse Beach Resto Virtual Reality",
+            url: "https://seasidejepara.digidaxa.com"
+        },
+        {
+            id: 4,
+            title: "Love in Hotel & Resort Jepara",
+            url: "https://loveinjepara.digidaxa.com"
+        }, 
+    ];
+const featuresData = ()=>[
+        {
+            id: 0,
+            icon: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_fa__WEBPACK_IMPORTED_MODULE_1__.FaVrCardboard, {}),
+            title: "Imersif 360, Visualisasi Makin Menawan",
+            content: "Libatkan customers dengan ruang pamer virtual yang sangat menarik."
+        },
+        {
+            id: 1,
+            icon: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_fa__WEBPACK_IMPORTED_MODULE_1__.FaBuffer, {}),
+            title: "Semakin Efisien",
+            content: "Uji produkmu di pasaran melalui virtual showroom 3D."
+        },
+        {
+            id: 2,
+            icon: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_fa__WEBPACK_IMPORTED_MODULE_1__.FaHandSparkles, {}),
+            title: "Tawarkan Pengalaman baru yang Menarik",
+            content: "Tawarkan produkmu dan bikin menarik agar mudah dikunjungi pelangganmu."
+        },
+        {
+            id: 3,
+            icon: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_fa__WEBPACK_IMPORTED_MODULE_1__.FaChartLine, {}),
+            title: "Tingkatkan Jangkauan",
+            content: "Jangkau pasar dan pelanggan semakin luas."
+        },
+        {
+            id: 4,
+            icon: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_fa__WEBPACK_IMPORTED_MODULE_1__.FaUserClock, {}),
+            title: "Akses 24/7",
+            content: "Melalui smartphone atau komputermu, akses mudah dan dapat dilakukan kapan saja dan di mana saja."
+        }, 
+    ];
+const virtualRealityData = ()=>[
+        {
+            id: 0,
+            title: "Photographed",
+            images: [
+                {
+                    id: 0,
+                    src: "/img/virtual-reality/photographed-1.webp",
+                    alt: "Photographed Virtual Reality"
+                },
+                {
+                    id: 1,
+                    src: "/img/virtual-reality/photographed-2.webp",
+                    alt: "Photographed Virtual Reality"
+                }, 
+            ],
+            content: "Photographed: Tangkap dan visualisasikan. Gunakan foto 360 apa saja, dan foto anda akan dapat langsung dilihat melalui panoramic view."
+        },
+        {
+            id: 1,
+            title: "Digitally Rendered",
+            images: [
+                {
+                    id: 0,
+                    src: "/img/virtual-reality/digitally-render-2.webp",
+                    alt: "Digitally Rendered Virtual Reality"
+                },
+                {
+                    id: 1,
+                    src: "/img/virtual-reality/digitally-render-1.webp",
+                    alt: "Digitally Rendered Virtual Reality"
+                }, 
+            ],
+            content: "Digitally Rendered: Buat ruang pamer virtual menggunakan CGI seperti kualitas film."
+        }, 
+    ];
+const benefitsData = ()=>[
+        {
+            id: 0,
+            title: "Visualisasi 3D Ruangan",
+            content: "Buat pengunjung semakin tertarik dan ingin merasakan sensasi tour ruangan yang elegan dan keren. Bawa mereka tour secara virtual untuk mengetahui fasilitas yang memukau dan berkelas pada hotel atau tempat rekreasimu. Penting untuk menjangkau pasar dan pelanggan lebih luas."
+        },
+        {
+            id: 1,
+            title: "Toko Virtual",
+            content: "Ciptakan sensasi belanja 3D yang nyata, buat pelanggan terkesan dengan perjalanan belanja interaktif dengan memanfaatkan imajinasi mereka, Hasilkan sesi pembelian yang jauh lebih lama dan tingkatkan konversi secara keseluruhan. Bisa kamu bangun dengan showroom berbasis 360 Photography atau Digitally Rendered."
+        },
+        {
+            id: 2,
+            title: "Visualisasi 3D Dekorasi Rumah",
+            content: "Buat pelanggan memvisualisasikan berbagai kombinasi ubin lantai & dinding, furnitur dan aksesori 3D, ciptakan pengalaman unik dan menarik serta mampu bangun kesan pelanggan tak terlupakan pada produkmu."
+        },
+        {
+            id: 3,
+            title: "Virtual Fashion Showroom",
+            content: "Bangun pengalaman berkesan dan lebih interaktif serta buat pelanggan makin tertarik dengan virtual fashion showroom."
+        }, 
+    ];
+const benefitButtonsData = ()=>[
+        {
+            id: 0,
+            name: "Untuk Hotel & Tempat Rekreasi"
+        },
+        {
+            id: 1,
+            name: "Untuk Ritel & Brand"
+        },
+        {
+            id: 2,
+            name: "Untuk Produsen & Properti"
+        },
+        {
+            id: 3,
+            name: "Untuk Merk & Grosir"
+        }, 
+    ];
+const ctaData = ()=>[
+        {
+            id: 0,
+            title: "Siap mengubah bisnis Anda dengan kami?",
+            text: "",
+            button: "Dapatkan Sekarang"
+        }, 
+    ];
+
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(171));
+module.exports = __webpack_exports__;
+
+})();
